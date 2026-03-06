5f98768e544ddc028e91e960ee8ccf2027467ffa
Chloe Morgan <77995029+Chloe-Abby-Morgan@users.noreply.github.com>
2026-03-06 18:02:09 +0000

    Jump Script
    Fuck physics

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,28 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float moveSpeed=5f;
+    [SerializeField] private float jumpUp=1f;
+    [SerializeField] private float jumpDown=2f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask whatIsGround;
+    public bool isGrounded=true;
     Vector2 Movement;
 
+    void Start()
+    {
+        isGrounded = true;
+    }
     void Update()
     {
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpUp * 1000f);
+        }
+
+         rb.gravityScale = rb.linearVelocityY > 0 ? jumpUp : jumpDown;
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.05f, whatIsGround);
         Movement.x = Input.GetAxisRaw("Horizontal");
         rb.MovePosition(rb.position + Movement * moveSpeed * Time.fixedDeltaTime);
     }
