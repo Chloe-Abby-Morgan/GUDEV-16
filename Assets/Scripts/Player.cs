e57e7981b9becd3d3c9e1793bfea0064932ac3b3
Chloe Morgan <77995029+Chloe-Abby-Morgan@users.noreply.github.com>
2026-03-10 04:40:18 +0000

    End of Day 3

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,40 +3,83 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Rigidbody2D baguetteHandle;
-    [SerializeField] private float moveSpeed=5f;
-    [SerializeField] private float jumpUp=1f;
-    [SerializeField] private float jumpDown=2f;
+    [SerializeField] private Rigidbody2D[] atackRange = new Rigidbody2D[3];
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpUp = 1f;
+    [SerializeField] private float jumpDown = 2f;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask whatIsGround;
-    private bool isGrounded=true;
-    private Camera cam;
-    Vector2 Movement;
-    Vector2 MousePos;
+    [SerializeField] private float dashSpeed = 20f;
+    [SerializeField] private float dashTime = 0.15f;
+    [SerializeField] private float dashCooldown = 0.5f;
+    private bool isGrounded = true;
+    private Vector2 Movement;
+    private bool isDashing = false;
+    private float dashTimer = 0f;
+    private float dashCooldownTimer = 0f;
+    private Vector2 dashDirection;
 
-    void Start()
-    {
-        isGrounded = true;
-        cam = Camera.main;
-    }
     void Update()
     {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.05f, whatIsGround);
 
-        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.AddForce(Vector2.up * jumpUp * 1000f);
         }
 
         rb.gravityScale = rb.linearVelocityY > 0 ? jumpUp : jumpDown;
 
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.05f, whatIsGround);
+        if (dashCooldownTimer > 0)
+        {
+            dashCooldownTimer -= Time.deltaTime;
+        }
 
-            MousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 lookDir = MousePos - baguetteHandle.position;
-            float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
-            baguetteHandle.rotation = angle;
-            Movement.x = Input.GetAxisRaw("Horizontal");
-            rb.MovePosition(rb.position + Movement * moveSpeed * Time.fixedDeltaTime);
-            baguetteHandle.position = rb.position;
+        if (!isDashing && dashCooldownTimer <= 0)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                BeginDash(Vector2.up);
+            }
+
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                BeginDash(Vector2.down);
+            }
+
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                BeginDash(Vector2.left);
+            }
+
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                BeginDash(Vector2.right);
+            }
+        }
+
+        if (isDashing)
+        {
+            dashTimer += Time.deltaTime;
+            rb.linearVelocity = dashDirection * dashSpeed;
+
+            if (dashTimer >= dashTime)
+            {
+                isDashing = false;
+                dashCooldownTimer = dashCooldown;
+            }
+
+            return; 
+        }
+
+        Movement.x = Input.GetAxisRaw("Horizontal");
+        rb.MovePosition(rb.position + Movement * moveSpeed * Time.deltaTime);
+    }
+
+    void BeginDash(Vector2 dir)
+    {
+        isDashing = true;
+        dashTimer = 0f;
+        dashDirection = dir.normalized;
     }
 }
