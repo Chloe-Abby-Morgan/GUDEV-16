8b18ef7b8b074a33ba6fc3f5bae691cbaa9c61fa
Chloe Morgan <77995029+Chloe-Abby-Morgan@users.noreply.github.com>
2026-03-11 03:31:28 +0000

    End of Day 4

--- Assets/Scripts/TimingManager.cs
+++ Assets/Scripts/TimingManager.cs
@@ -1,22 +1,30 @@
 using System.CodeDom.Compiler;
+using System.Collections;
 using UnityEngine;
 
 public class TimingManager : MonoBehaviour
 {
     [SerializeField] private GameObject cardUI;
+    [SerializeField] private float waitTime=5f;
     private bool generated;
+    private bool waiting=false;
     public bool showingUI=true;
     public CardManager cM;
 
     void Update()
     {
+        if(!waiting && !showingUI)
+        {
+            StartCoroutine(Display());
+            waiting = true;
+        }
         if(showingUI)
         {
             if(!generated)
             {
-            cardUI.SetActive(true);
-            cM.SpawnRandomButtons(3);
-            generated = true;
+                cardUI.SetActive(true);
+                cM.SpawnRandomButtons(3);
+                generated = true;
             }
         }
         else
@@ -24,5 +32,12 @@ public class TimingManager : MonoBehaviour
             generated = false;
             cardUI.SetActive(false);
         }
+    }
+
+    IEnumerator Display()
+    {
+        yield return new WaitForSeconds(waitTime);
+        showingUI = true;
+        waiting = false;
     }
 }
